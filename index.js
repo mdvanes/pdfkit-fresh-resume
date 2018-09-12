caf685e952d65547cc65d8ae1dff30cc28e63f2f
M.D. van Es <es@mdworld.nl>
2018-09-12 09:05:44 +0200

    Parse args with yargs

--- index.js
+++ index.js
@@ -6,15 +6,54 @@ const fs = require('fs');
 // const fsPromise = require('../util/fsPromise');
 // const DataURI = require('datauri').promise;
 const resumeTemplate = require('./app/template/resume');
-const pa = require('./app/util/parseArguments');
 
-const inputJson = require(`./${pa.inputPath}`);
-const outputPath = `./output/${pa.outputName}.pdf`;
-const doc = new PDFDocument();
-// const stream =
-doc.pipe(fs.createWriteStream(outputPath));
+// https://github.com/yargs/yargs/blob/master/docs/examples.md
+// eslint-disable-next-line no-unused-vars
+const argv = require('yargs')
+    .usage('$0 <cmd> [args]')
+    .option('watch', {
+        alias: 'w',
+        describe: 'Description for watch option'
+    })
+    .option('template', {
+        alias: 't',
+        describe: 'Description for template option'
+    })
+    .version() // taken from package.json
+    .command('run [input] [output]', 'Run the transformation from JSON to PDF', (yargs) => {
+        yargs.positional('input', {
+            type: 'string',
+            // default: 'Cambi',
+            describe: 'The path to the input JSON, with extension'
+        });
+        yargs.positional('output', {
+            type: 'string',
+            // default: 'Cambi',
+            describe: 'The filename for the output PDF, with extension'
+        });
+    }, function (argv) {
+        console.log(`Trying to run with input ${argv.input} and output ${argv.output}`);
+        // const pa = require('./app/util/parseArguments');
+        // Example: node index.js run example-resume.json example-output.pdf -t -w
+        // TODO check if input exists
+        const inputJson = JSON.parse(fs.readFileSync(argv.input)); // require(`./${pa.inputPath}`);
+        // TODO check if argv.output ends in PDF
+        const outputPath = argv.output; // `./output/${pa.outputName}.pdf`;
+        const doc = new PDFDocument();
+        // const stream =
+        doc.pipe(fs.createWriteStream(outputPath));
 
-resumeTemplate.addContent(doc, inputJson);
+        if(argv.watch) {
+            console.log('Flag "watch" is not yet implemented');
+        }
+        if(argv.template) {
+            console.log('Flag "template" is not yet implemented');
+        }
+
+        resumeTemplate.addContent(doc, inputJson);
+    })
+    .help()
+    .argv;
 
 // // It's probably not wise to convert this to a promise, because then the function on a higher level should be made async
 // // Also there are no (error, result) params on the callback
