c1a1499016bc3da6d039ba26e24cf110870ac0dc
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-09-12 11:42:34 +0200

    Improve example

--- app/template/defaultTemplate.js
+++ app/template/defaultTemplate.js
@@ -1,15 +1,59 @@
 #!/usr/bin/env node
 /* eslint-env node */
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.';
 
-function addContent(doc, inputJson) {
+const addEmploymentHistory = (doc, inputJson) => {
+    doc
+        .moveDown(1.5)
+        .fontSize(25)
+        .font('Helvetica-Bold')
+        .text('Employment');
 
+    inputJson.employment.history.forEach(employmentItem => {
+        const dates = employmentItem.end
+            ? `From ${employmentItem.start} to ${employmentItem.end}`
+            : `From ${employmentItem.start}`;
+        doc
+            .moveDown(0.5)
+            .fontSize(16)
+            .font('Helvetica-Bold')
+            .text(`${employmentItem.position} at ${employmentItem.employer}`)
+            .fontSize(12)
+            .font('Helvetica')
+            .text(`${employmentItem.url}`)
+            .text(dates)
+            .text(`${employmentItem.summary}`)
+            .text(`Keywords: ${employmentItem.keywords.join(' | ')}`)
+            .text(`Highlights: ${employmentItem.highlights.join('; ')}`);
+    });
+};
+
+const addExampleColumns = (doc) => {
+    // Multiple columns
+    doc
+        .text(lorem, {
+            columns: 3,
+            columnGap: 15,
+            height: 150,
+            width: 465,
+            align: 'justify'
+        })
+        .moveDown(0.5);
+};
+
+function addContent(doc, inputJson, inputDir) {
+    // Set font defaults
+    doc
+        .fontSize(12)
+        .font('Helvetica');
+
+    // Add primary personal information
     doc.text(`Name: ${inputJson.name}`, 50, 120);
     doc.text(`Role: ${inputJson.info.characterClass}`);
-    doc.text(`Employment: ${inputJson.employment.history}`);
-    inputJson.employment.history.forEach(employmentItem => {
-        doc.text(`Employer: ${employmentItem.employer}`);
-    });
 
+    addEmploymentHistory(doc, inputJson);
+
+    // Add a random shape
     doc.save()
        .moveTo(500, 650)
        .lineTo(400, 750)
@@ -20,8 +64,12 @@ function addContent(doc, inputJson) {
     if(inputJson.info && inputJson.info.image) {
         doc.circle(300, 70, 40)
             .clip()
-            .image(inputJson.info.image, 260, 30, { width: 80 });
+            .image(`${inputDir}${inputJson.info.image}`, 260, 30, { width: 80 });
     }
+
+    doc.addPage();
+
+    addExampleColumns(doc);
 
     doc.end();
 }
